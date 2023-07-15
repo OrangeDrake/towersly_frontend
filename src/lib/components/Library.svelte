ff515cc31687b9667b37f532fc94c3a19eff71a6
Petr Fiala <petrfiala108@gmail.com>
2023-07-15 08:38:10 +0200

    skeleton

--- src/lib/components/Library.svelte
+++ src/lib/components/Library.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { shelves } from '$lib/stores/libraryStore.js';
+  import { shelves } from "$lib/stores/libraryStore.js";
   import { onMount } from "svelte";
 
   const getData = async () => {
@@ -19,26 +19,43 @@
     console.log(response);
     const data = await response.json();
     console.log(data);
-    $shelves = data; 
+    $shelves = data;
   };
 
   onMount(() => {
-    getData()
+    getData();
   });
 </script>
 
 {#if $shelves == null}
   <div>Loading Shelves...</div>
 {:else}
-  {#each Object.values($shelves) as shelf, i}
-    <h1>
-      {shelf.name}
-    </h1>
-    <h3>Works:</h3>
-    {#each Object.values(shelf.works) as work, j}
-      <p>
-        {"- " + j + " " + work.name}
-      </p>
+  <div class="library">
+    {#each Object.values($shelves) as shelf, i}
+      <div class="shelf">
+        <h1>
+          {shelf.name}
+        </h1>
+        <h3>Works:</h3>
+        {#each Object.values(shelf.works) as work, j}
+          <p>
+            {"- " + j + " " + work.name}
+          </p>
+        {/each}
+      </div>
     {/each}
-  {/each}
+  </div>
 {/if}
+
+<style>
+  .library {
+    display: flex;
+  }
+  .shelf {  
+    flex-wrap: wrap;
+    width: 400px;
+    padding: 5px;
+    margin: 5px;
+    background-color:#fff79e;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import '../app.css';
+
+</script>
+
+<h1>Layout TOP</h1>
+<slot />
+<h1>Layout BOTTOM</h1>
