4dedd9b6c702af31b673fd7422bc47112c56b3fd
Petr Fiala <petrfiala108@gmail.com>
2023-12-03 21:06:55 +0100

    login moved to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,122 @@
 <script>
   import { onMount } from "svelte";
-//   import { initializeStores } from "@skeletonlabs/skeleton";
-  import { Toast, toastStore  } from '@skeletonlabs/skeleton';
-  // Your selected Skeleton theme:
+  import Keycloak from "keycloak-js";
+  import { Toast, toastStore } from "@skeletonlabs/skeleton";
   import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
-
-  // This contains the bulk of Skeletons required styles:
   import "@skeletonlabs/skeleton/styles/skeleton.css";
-
-  // Finally, your application's global stylesheet (sometimes labeled 'app.css')
   import "../app.css";
 
-//   initializeStores();
+  import { shelves } from "$lib/stores/libraryStore.js";
+  import { distributions } from "$lib/stores/planningStore.js";
+  import { keycloak } from "$lib/stores/keycloakStore.js";
+  import { API_URL } from "$lib/components/Constants.svelte";
+  import { tracking, trackTime } from "$lib/stores/timeTrackingStore.js";
 
-  const action = (node) => {
-    console.log("******************action layout");
+  let loginState = "waiting for login...";
+
+  const getShelves = async () => {
+    const token_value = "Bearer " + $keycloak.token;
+    console.log("URL" + API_URL);
+    var response = await fetch(API_URL + "/library", {
+      method: "GET",
+      headers: {
+        Authorization: token_value,
+      },
+    });
+
+    const data = await response.json();
+
+    $shelves = data;
   };
 
-	onMount(() => {
-    console.log("******************onMount layout");
-    
-		window.onunhandledrejection = (e) => {
-    console.log("+++++++++++++++error");
-    }
-  });
+  const getDistributions = async () => {
+    const token_value = "Bearer " + $keycloak.token;
 
+    var response = await fetch(API_URL + "/planning", {
+      method: "GET",
+      headers: {
+        Authorization: token_value,
+      },
+    });
+
+    const data = await response.json();
+
+    $distributions = data;
+  };
+
+  const getTimeTracking = async () => {
+    const token_value = "Bearer " + $keycloak.token;
+
+    var response = await fetch(API_URL + "/profile/tracking", {
+      method: "GET",
+      headers: {
+        Authorization: token_value,
+      },
+    });
+
+    console.log("##########response: " + JSON.stringify(response));
+
+    // if (!Object.keys(response.body).length) {
+    //   console.log("##########tracking empty ");
+    //   $tracking = null;
+    //   return;
+    // }
+
+    const data = await response.json();
+    console.log("##########tracking: " + JSON.stringify($tracking));
+
+    if (!Object.keys(data).length) {
+      console.log("##########tracking empty ");
+      return;
+    }
+
+    $tracking = data;
+    trackTime();
+
+    //   if (tracking != null) {
+    //     console.log("#######tracking started");
+    //     trackTime();
+    //   }
+  };
+
+  async function initKeycloak() {
+    $keycloak = new Keycloak({
+      url: "http://localhost:8080",
+      realm: "master",
+      clientId: "t_client",
+    });
+    $keycloak
+      .init({
+        onLoad: "login-required",
+      })
+      .then((auth) => {
+        if (auth) {
+          loginState = "logged in";
+          getShelves();
+          getDistributions();
+          getTimeTracking();
+        }
+      })
+      .catch(function () {
+        alert("failed to initialize");
+      });
+
+    // try {
+    //   const authenticated = await $keycloak.init();
+    //   console.log(`User is ${authenticated ? "authenticated" : "not authenticated"}`);
+    // } catch (error) {
+    //   console.error("Failed to initialize adapter:", error);
+    // }
+  }
+
+  onMount(() => {
+    initKeycloak();
+    console.log("*********************onMount page");
+  });
 </script>
 
-<h1 use:action>Layout TOP &#E2;</h1>
+<div>{loginState}</div>
 <slot />
+
 <h1>Layout BOTTOM</h1>
-<Toast position = "t" />
+<Toast position="t" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
   import { afterUpdate } from "svelte";
-  import { shelves } from "$lib/stores/libraryStore.js";
-  import { distributions } from "$lib/stores/planningStore.js";
-  import { keycloak } from "$lib/stores/keycloakStore.js";
-  import Keycloak from "keycloak-js";
-  import { onMount } from "svelte";
 
   import Library from "$lib/components/Library.svelte";
   import Planning from "$lib/components/Planning.svelte";
@@ -13,11 +8,8 @@
   import Connection2 from "$lib/components/Connection2.svelte";
   import Calendar from "$lib/components/Calendar.svelte";
 
-  import { API_URL } from "$lib/components/Constants.svelte";
   import { reDrawCurves } from "$lib/stores/connectionStore.js";
-
-  import { tracking, trackTime } from "$lib/stores/timeTrackingStore.js";
-
+ 
   afterUpdate(() => {
     console.log("************page updated*******");
   });
@@ -26,133 +18,12 @@
   let end1;
   let end2;
 
-  // const drawLines = async () => {
-  //   console.log("line drawing");
-  //   console.log("start: " + Object.values(start1));
-  //   console.log("end: " + Object.values(end1));
-  //   var line;
-  //   console.log("line before: " + line)
-  //   line = new LeaderLine(start1, end2);
-  //   console.log("line after: " + Object.values(line))
-
-  //   line.color = "red"; // Change the color to red.
-  //   line.size++; // Up size.
-  //   console.log(line.size);
-  //   console.log("end line drawing");
-  // };
-
-  let loginState = "waiting for login...";
-
-  const getShelves = async () => {
-    const token_value = "Bearer " + $keycloak.token;
-    console.log("URL" + API_URL);
-    var response = await fetch(API_URL + "/library", {
-      method: "GET",
-      headers: {
-        Authorization: token_value,
-      },
-    });
-
-    const data = await response.json();
-
-    $shelves = data;
-  };
-
-  const getDistributions = async () => {
-    const token_value = "Bearer " + $keycloak.token;
-
-    var response = await fetch(API_URL + "/planning", {
-      method: "GET",
-      headers: {
-        Authorization: token_value,
-      },
-    });
-
-    const data = await response.json();
-
-    $distributions = data;
-  };
-
-  const getTimeTracking = async () => {
-    const token_value = "Bearer " + $keycloak.token;
-
-    var response = await fetch(API_URL + "/profile/tracking", {
-      method: "GET",
-      headers: {
-        Authorization: token_value,
-      },
-    });
-
-    console.log("##########response: " + JSON.stringify(response));
-
-    // if (!Object.keys(response.body).length) {
-    //   console.log("##########tracking empty ");
-    //   $tracking = null;
-    //   return;
-    // }
-
-    const data = await response.json();
-    console.log("##########tracking: " + JSON.stringify($tracking));
-
-    if (!Object.keys(data).length) {
-      console.log("##########tracking empty ");
-      return;
-    }
-
-    $tracking = data;
-    trackTime();
-
-    //   if (tracking != null) {
-    //     console.log("#######tracking started");
-    //     trackTime();
-    //   }
-  };
-
-  async function initKeycloak() {
-    $keycloak = new Keycloak({
-      url: "http://localhost:8080",
-      realm: "master",
-      clientId: "t_client",
-    });
-    $keycloak
-      .init({
-        onLoad: "login-required",
-      })
-      .then((auth) => {
-        if (auth) {
-          loginState = "logged in";
-          getShelves();
-          getDistributions();
-          getTimeTracking();
-        }
-      })
-      .catch(function () {
-        alert("failed to initialize");
-      });
-
-    // try {
-    //   const authenticated = await $keycloak.init();
-    //   console.log(`User is ${authenticated ? "authenticated" : "not authenticated"}`);
-    // } catch (error) {
-    //   console.error("Failed to initialize adapter:", error);
-    // }
-  }
-
-  onMount(() => {
-    initKeycloak();
-    console.log("*********************onMount page");
-  });
 </script>
-
-<!-- <svelte:head>
-  <script src="leader-line.min.js" on:load={initializeRemarkable}></script> -->
-<!-- <script src="leader-line.min.js" on:load={initializeRemarkable}></script>
-  <script src="leader-line.min.js"></script>
-</svelte:head> -->
-<div>{loginState}</div>
 
 {#key $reDrawCurves}
   <div class="w-max">
+
+
     <Connection /> musí být první aby nepřekrýval popupy
     <Library />
     <ConnectionBackground />
