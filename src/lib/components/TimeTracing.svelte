2ad9b63ea96f6b5f438c991768e7f7fd80fa2d80
Petr Fiala <petr.fiala@atlas.cz>
2024-01-05 19:37:34 +0100

    Rename Button

--- src/lib/components/TimeTracing.svelte
+++ src/lib/components/TimeTracing.svelte
@@ -129,7 +129,7 @@
       <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M10 6v4l3.276 3.276M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
     </svg>
 
-    <span class="text-lg">Select work to Track</span></button
+    <span class="text-lg">Track Progeress</span></button
   >
   <!-- {/if} -->
 
