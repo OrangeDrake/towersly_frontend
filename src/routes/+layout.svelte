7ffff46a340afdf1407942bda555585b1d3d2b75
Petr Fiala <petr.fiala@atlas.cz>
2024-01-22 20:09:14 +0100

    update token and create profil if needed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,117 +27,126 @@
     };
 
 
-        const getShelves = async () => {
-            console.log("--------------------getShelves");
-            const token_value = "Bearer " + $keycloak.token;
-            console.log("URL" + PUBLIC_API_URL);
-            var response = await fetch(PUBLIC_API_URL + "/library", {
-                method: "GET",
-                headers: {
-                    Authorization: token_value,
-                },
-            });
+    const getShelves = async () => {
+        console.log("--------------------getShelves");
+        const token_value = "Bearer " + $keycloak.token;
+        console.log("URL" + PUBLIC_API_URL);
+        var response = await fetch(PUBLIC_API_URL + "/library", {
+            method: "GET",
+            headers: {
+                Authorization: token_value,
+            },
+        });
 
-            const data = await response.json();
+        const data = await response.json();
 
-            $shelves = data;
-        };
+        $shelves = data;
+    };
 
-        const getDistributions = async () => {
-            const token_value = "Bearer " + $keycloak.token;
+    const getDistributions = async () => {
+        const token_value = "Bearer " + $keycloak.token;
 
-            var response = await fetch(PUBLIC_API_URL + "/planning", {
-                method: "GET",
-                headers: {
-                    Authorization: token_value,
-                },
-            });
+        var response = await fetch(PUBLIC_API_URL + "/planning", {
+            method: "GET",
+            headers: {
+                Authorization: token_value,
+            },
+        });
 
-            const data = await response.json();
+        const data = await response.json();
 
-            $distributions = data;
-        };
+        $distributions = data;
+    };
 
-        const getTimeTracking = async () => {
-            const token_value = "Bearer " + $keycloak.token;
+    const getTimeTracking = async () => {
+        const token_value = "Bearer " + $keycloak.token;
 
-            var response = await fetch(PUBLIC_API_URL + "/profile/tracking", {
-                method: "GET",
-                headers: {
-                    Authorization: token_value,
-                },
-            });
+        var response = await fetch(PUBLIC_API_URL + "/profile/tracking", {
+            method: "GET",
+            headers: {
+                Authorization: token_value,
+            },
+        });
 
-            console.log("##########response: " + JSON.stringify(response));
+        console.log("##########response: " + JSON.stringify(response));
 
-            const data = await response.json();
-            console.log("##########tracking: " + JSON.stringify($tracking));
+        const data = await response.json();
+        console.log("##########tracking: " + JSON.stringify($tracking));
 
-            if (!Object.keys(data).length) {
-                console.log("##########tracking empty ");
-                return;
-            }
-
-            $tracking = data;
-            if ($tracking != null) {
-                $reDrawCurves = "time tracking stop: " + Date.now();
-            }
-            trackTime();
-        };
-
-        const getNumberOfVisibleWork = async () => {
-            const token_value = "Bearer " + $keycloak.token;
-
-            var response = await fetch(PUBLIC_API_URL + "/settings/worksnumber", {
-                method: "GET",
-                headers: {
-                    Authorization: token_value,
-                },
-            });
-
-            const data = await response.json();
-            $numberOfVisibleWork = data;
-        };
-
-        async function initKeycloak() {
-            $keycloak = new Keycloak({
-                url: PUBLIC_KEYCLOAK_URL,
-                realm: "master2",
-                clientId: "t_client",
-            });
-            $keycloak
-                .init({
-                    onLoad: "login-required",
-                })
-                .then((auth) => {
-                    if (auth) {
-                        loginState = "logged in";
-                        creteUserIfneeded().then(() => {
-                            getShelves();
-                            getDistributions();
-                            getTimeTracking();
-                            getNumberOfVisibleWork()
-                            console.log("created if needed")
-                        });
-                    }
-                })
-                .catch(function () {
-                    // alert("failed to initialize");
-                    console.log("failed to initialize");
-                });
-
-            // try {
-            //   const authenticated = await $keycloak.init();
-            //   console.log(`User is ${authenticated ? "authenticated" : "not authenticated"}`);
-            // } catch (error) {
-            //   console.error("Failed to initialize adapter:", error);
-            // }
+        if (!Object.keys(data).length) {
+            console.log("##########tracking empty ");
+            return;
         }
 
-        onMount(() => {
-            initKeycloak();
-            console.log("*********************onMount page");
+        $tracking = data;
+        if ($tracking != null) {
+            $reDrawCurves = "time tracking stop: " + Date.now();
+        }
+        trackTime();
+    };
+
+    const getNumberOfVisibleWork = async () => {
+        const token_value = "Bearer " + $keycloak.token;
+
+        var response = await fetch(PUBLIC_API_URL + "/settings/worksnumber", {
+            method: "GET",
+            headers: {
+                Authorization: token_value,
+            },
         });
+
+        const data = await response.json();
+        $numberOfVisibleWork = data;
+    };
+
+    async function initKeycloak() {
+        $keycloak = new Keycloak({
+            url: PUBLIC_KEYCLOAK_URL,
+            realm: "master2",
+            clientId: "t_client",
+        });
+        $keycloak
+            .init({
+                onLoad: "login-required",
+            })
+            .then((auth) => {
+                if (auth) {
+                    loginState = "logged in";
+                    creteUserIfneeded().then(() => {
+                        getShelves();
+                        getDistributions();
+                        getTimeTracking();
+                        getNumberOfVisibleWork()
+                        console.log("created if needed")
+                    });
+                }
+            })
+            .catch(function () {
+                // alert("failed to initialize");
+                console.log("failed to initialize");
+            });
+
+        // try {
+        //   const authenticated = await $keycloak.init();
+        //   console.log(`User is ${authenticated ? "authenticated" : "not authenticated"}`);
+        // } catch (error) {
+        //   console.error("Failed to initialize adapter:", error);
+        // }
+    }
+
+    onMount(() => {
+        initKeycloak();
+        console.log("*********************onMount page");
+
+        async function updateToken() {
+            console.log("---update token");
+            $keycloak.updateToken(10*60);
+        }
+        const interval = setInterval(updateToken, 9*60 * 1000);
+        return () => clearInterval(interval);
+    });
+
+
 </script>
 
 <div class="font-semibold bg-white">
